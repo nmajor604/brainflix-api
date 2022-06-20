66fc5774744b37821f847f8caa64d56602eedfa3
nmajor604 <81140580+nmajor604@users.noreply.github.com>
2022-06-19 22:11:52 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,9 @@
+// initialize Express in project
+const express = require('express');
+const app = express();
+
+// start Express on port 8080
+app.listen(8080, () => {
+    console.log('Server Started on http://localhost:8080');
+    console.log('Press CTRL + C to stop server');
+});
